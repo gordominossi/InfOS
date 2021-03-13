56e52bf5cd7aab97616d2383bdaaa7278d9d3421
Gabriel Moreira Minossi <gordominossi@gmail.com>
2021-03-13 14:15:43 -0300

    Moving overview logic to its own file. Renaming a few variables

--- Programs/monitor-system/api/gui/overview.lua
+++ Programs/monitor-system/api/gui/overview.lua
@@ -1,3 +1,9 @@
+-- Import section
+
+local multiBlockAddresses = require("config.addresses.multi-blocks")
+
+--
+
 --[[
 |gla| overview  |
 |wid| w | w | w |
@@ -5,7 +11,33 @@
 |sto| w | w | w |
 |not| power |b|f|
 --]]
-
-return {
-    title = "Overview"
+local overview = {
+    title = "Overview",
+    pageIndex = 1,
+    widgets = {
+        active = {}
+    }
 }
+
+for name, address in pairs(multiBlockAddresses) do
+    table.insert(overview.widgets, Widget.createMachineWidget(address, name))
+end
+
+function overview.widgets.update()
+    for i = 1, 9 do
+        overview.widgets.active[i] = overview.widgets[9 * (overview.pageIndex - 1) + i]
+    end
+    Widget.clear()
+end
+
+function overview.update()
+    overview.widgets.update()
+end
+
+function overview.draw()
+    for index, activeWidget in ipairs(overview.widgets.active) do
+        activeWidget:draw(index)
+    end
+end
+
+return overview
--- Programs/monitor-system/init.lua
+++ Programs/monitor-system/init.lua
@@ -1,16 +1,20 @@
 -- Import section
 
 GUI = require("api.gui")
-
--- local cleanroomAddresses = require("config.addresses.cleanroom")
-local multiBlockAddresses = require("config.addresses.multi-blocks")
-local energyBufferAddresses = require("config.addresses.energy-buffers")
+-- GUI = require("api.power")
+-- GUI = require("api.machine")
+-- GUI = require("api.notification")
+-- GUI = require("api.stock")
 
 --
 
-GUI.setup(energyBufferAddresses.batteryBuffer1, multiBlockAddresses)
+GUI.setup()
 
 while true do
     GUI.update()
+    -- Power.update()
+    -- Machines.update()
+    -- Notifications.update()
+    -- Stock.update()
     os.sleep(0)
 end
