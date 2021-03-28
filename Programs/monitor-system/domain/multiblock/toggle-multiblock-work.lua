a7bf4fe5769401133a8b13ee15311c9985d9f157
Gabriel Moreira Minossi <gordominossi@gmail.com>
2021-03-28 00:14:36 -0300

    Simplifying machine/mock relations Creating mock methods for machine when it is not found

--- Programs/monitor-system/domain/multiblock/toggle-multiblock-work.lua
+++ Programs/monitor-system/domain/multiblock/toggle-multiblock-work.lua
@@ -6,7 +6,7 @@ Alarm = require("api.sound.alarm")
 local function exec(address, name)
     local multiblock = Machine.getMachine(address, name, Machine.types.singleblock)
     local workAllowed = multiblock:isWorkAllowed()
-    multiblock:setWorkAllowed(not workAllowed)
+    multiblock.setWorkAllowed(not workAllowed, multiblock)
 end
 
 return exec
